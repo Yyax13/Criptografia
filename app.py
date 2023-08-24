d1fcf301c268085635814cced27d86cd7e8b6ace
heyDouglas <doguiyt@gmail.com>
2023-08-23 21:44:12 -0300

    start
    start

---
+++ app.py
@@ -0,0 +1,9 @@
+print("#######################################")
+print("#                                     #")
+print("#    CRIPTOGRAFIA - CIFRA DE CESAR    #")
+print("#                                     #")
+print("#######################################")
+
+
+pular_casas = input("Digite o número de casas que deseja pular: ")
+print("Você escolheu pular: " + pular_casas)
