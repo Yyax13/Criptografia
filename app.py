3ea2378609440798f764570e0971a0ab597bea16
heyDouglas <doguiyt@gmail.com>
2023-08-23 21:58:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ print("#    CRIPTOGRAFIA - CIFRA DE CESAR    #")
 print("#                                     #")
 print("#######################################")
 
+print("[1] Criptografar")
+print("[2] Descriptografar")
+print("[3] Informações")
+print("[4] Sair")
+opcao = input("Escolha a opção desejada: ")
 
-deslocamento = input("Escolha um valor de deslocamento: ")
-print("Você escolheu: " + deslocamento)
+
+if opcao == 1:
+    deslocamento = input("Escolha um valor de deslocamento: ")
+    print("Você escolheu: " + deslocamento)
+
+elif opcao == 2:
+    descrip = input("Qual a chave de deslocamento: ")
+    print("Você escolheu: " + descrip)
+
+elif opcao == 3:
+    print("Em criptografia, a Cifra de César, também conhecida como cifra de troca, código de César ou troca de César, é uma das mais simples e conhecidas técnicas de criptografia.")
+
+elif opcao == 4:
+    exit()
